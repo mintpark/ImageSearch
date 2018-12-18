23102ad61313fbf903372774ae3c08f1b624c1db
mint <hy.park@memebox.com>
2018-12-18 21:56:43 +0900

    initialize & add third-parties

---
+++ ImageSearchTests/ImageSearchTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImageSearchTests.swift
+//  ImageSearchTests
+//
+//  Created by Hayoung Park on 18/12/2018.
+//  Copyright © 2018 Hayoung Park. All rights reserved.
+//
+
+import XCTest
+@testable import ImageSearch
+
+class ImageSearchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'ImageSearch' do
+    pod 'Alamofire', '~> 5.0.0.beta.1'
+    pod 'Kingfisher', '~> 5.0'
+
+end
